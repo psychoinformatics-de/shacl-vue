061d07ac69b19c9a6e2738db7030fe6a44b8d89a
Stephan Heunis <s.heunis@fz-juelich.de>
2024-06-20 16:55:30 +0200

    new standalone io module depending on fetch-lite and providing methods for reading and writing rdf data

---
+++ src/modules/io.js
@@ -0,0 +1,33 @@
+/**
+ * @module io.js
+ * @description This module provides common functionality for reading and writing
+ * to files or endpoints
+ * 
+ * It depends mainly on https://github.com/rdfjs-base/fetch-lite for reading RDF data
+ * into streams and writing stream data to serialized RDF formats
+ */
+
+
+import formats from '@rdfjs/formats-common'
+import fetch from '@rdfjs/fetch-lite'
+
+export async function readRDF(file_url) {
+    const res = await fetch(file_url, { formats })
+    const quadStream = await res.quadStream()
+    // quadStream.on('error', err => console.error(err))
+    return quadStream
+    // Examples of what to do with the quadstream:
+    // 
+    //   - quadStream.on('error', err => console.error(err))
+    //   - quadStream.on('prefix', (prefix, ns) => {
+    //         console.log(`prefix: ${prefix} ${ns.value}`)
+    //     })
+    //   - quadStream.on('data', quad => {
+    //         console.log(`${quad.subject.value} - ${quad.predicate.value} -  ${quad.object.value}`)
+    //     })
+    //   -  quadStream.on('prefix', (prefix, ns) => {
+    //          // do something for every prefix
+    //      }).on('end', () => {
+    //          // do something at the end of all prefixes
+    //      })
+}
