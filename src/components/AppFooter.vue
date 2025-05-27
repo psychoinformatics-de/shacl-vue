68a5d921c052ee2d037acf367b735210194136b9
Stephan Heunis <s.heunis@fz-juelich.de>
2025-05-27 15:06:32 +0200

    Get git branch and commit at build time and add to app footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,7 @@
     >
       <span class="d-none d-sm-inline-block">Powered by</span>
       <a      
-      href="item.href"
+      :href="link"
       title="shacl-vue"
       class="d-inline-block mx-2 social-link"
       rel="noopener noreferrer"
@@ -14,10 +14,16 @@
     >
       <v-icon size="24"><v-img src="/shacl_vue_logo.svg"></v-img></v-icon>
     </a>
-    <span class="d-none d-sm-inline-block">shacl-vue</span>
+    <span class="d-none d-sm-inline-block">shacl-vue
+      (<a target="_blank" :href="link">{{ branch + '@' + commit_short }}</a>)
+    </span>
     </div>
   </v-footer>
 </template>
 
 <script setup>
+  const branch = __BRANCH__
+  const commit_short = __COMMIT_HASH_SHORT__
+  const commit = __COMMIT_HASH__
+  const link = `https://github.com/psychoinformatics-de/shacl-vue/tree/${commit}`
 </script>
