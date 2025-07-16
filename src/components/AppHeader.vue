2154b56b2df8e637636d1aa506b3faa007e28929
Stephan Heunis <s.heunis@fz-juelich.de>
2025-07-16 16:50:41 +0200

    Introduce file uploader credentials
    This uses the updated token composable in order to store
    credentials, in session storage, for the username and password
    of a user to allow git annex p2phttp based file upload

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,8 +19,20 @@
         </v-app-bar-title>
 
         <template v-slot:append>
+            <span v-if="configVarsMain.useGitannexP2phttp">
+                <v-tooltip text="File upload credentials" location="bottom">
+                    <template v-slot:activator="{ props }">
+                        <v-btn
+                            icon="mdi-file-key"
+                            @click="fileUploadFn()"
+                            v-bind="props"
+                            :disabled="!canSubmit"
+                        ></v-btn>
+                    </template>
+                </v-tooltip>
+            </span>
             <span v-if="configVarsMain.useToken">
-                <v-tooltip text="Token" location="bottom">
+                <v-tooltip text="Submission token" location="bottom">
                     <template v-slot:activator="{ props }">
                         <v-badge
                             v-if="http401response"
@@ -157,16 +169,78 @@
             </v-card-actions>
         </v-card>
     </v-dialog>
+
+    <v-dialog v-model="tokenDialogCreds" max-width="700">
+        <v-card>
+            <v-card-title>File Upload Credentials</v-card-title>
+            <v-card-text>
+                A username and password (access token) are required in order to upload
+                files to the configured storage backend at
+                <a :href="configVarsMain.gitannexP2phttpConfig.deployment_url" target="_blank">{{ configVarsMain.gitannexP2phttpConfig.deployment_url }}</a>.
+                <br /><br />
+
+                <span v-if="configVarsMain.gitannexP2phttpConfig.info">
+                    {{ configVarsMain.gitannexP2phttpConfig.info
+                    }}<span v-if="configVarsMain.gitannexP2phttpConfig.info_url"
+                        >:
+                        <a target="_blank" :href="configVarsMain.gitannexP2phttpConfig.info_url"
+                            >link</a
+                        ></span
+                    >
+                    <br /><br />
+                </span>
+
+                Below you can enter/update your username and password:
+                <br><br>
+                <v-form
+                    ref="tokenFormCreds"
+                    validate-on="submit lazy"
+                    @submit.prevent
+                >
+                    <v-text-field
+                        v-model="tokenvalUsr"
+                        :rules="rules"
+                        type="text"
+                        density="compact"
+                        placeholder="username"
+                        variant="outlined"
+                    ></v-text-field>
+                    <v-text-field
+                        v-model="tokenvalPwd"
+                        :rules="rules"
+                        :append-inner-icon="visiblePwd ? 'mdi-eye-off' : 'mdi-eye'"
+                        :type="visiblePwd ? 'text' : 'password'"
+                        density="compact"
+                        placeholder="password"
+                        prepend-inner-icon="mdi-lock-outline"
+                        variant="outlined"
+                        @click:append-inner="visiblePwd = !visiblePwd"
+                    ></v-text-field>
+                </v-form>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn @click="cancelCreds()"
+                    ><v-icon>mdi-close</v-icon> Cancel</v-btn
+                >
+                <v-btn @click="resetCreds()"><v-icon>mdi-undo</v-icon> Reset</v-btn>
+                <v-btn type="submit" @click="saveCreds()"
+                    ><v-icon>mdi-check-circle-outline</v-icon> Save</v-btn
+                >
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 <script setup>
-import { inject, onBeforeMount, ref, watch } from 'vue';
-import { useToken } from '@/composables/tokens';
+import { inject, onBeforeMount, ref, watch, computed } from 'vue';
+import { useTokens } from '@/composables/tokens';
 
 const props = defineProps({
     logo: String,
 });
 
-const { token, setToken, clearToken } = useToken();
+const { tokens, setToken, clearToken } = useTokens();
+const tokenName = 'serviceToken';
+const token = tokens[tokenName];
 const submitFn = inject('submitFn');
 const canSubmit = inject('canSubmit');
 const nodesToSubmit = inject('nodesToSubmit');
@@ -178,10 +252,20 @@ const tokenDialog = ref(false);
 const tokenExists = ref(false);
 const tokenval = ref('');
 const tokenForm = ref(null);
+
+const tokenNameUsr = 'gitAnnexUserName'
+const tokenNamePwd = 'gitAnnexPassword'
+const visibleUsr = ref(false);
+const visiblePwd = ref(false);
+const tokenDialogCreds = ref(false);
+const tokenExistsCreds = ref(false);
+const tokenvalUsr = ref('');
+const tokenvalPwd = ref('');
+const tokenFormCreds = ref(null);
 const rules = [
     (value) => {
         if (value) return true;
-        return 'A token is required';
+        return 'A value is required';
     },
 ];
 const customError = ref('')
@@ -192,12 +276,56 @@ onBeforeMount(() => {
         tokenExists.value = true;
         tokenval.value = token.value;
     }
+
+    if (tokens[tokenNameUsr].value !== null && tokens[tokenNameUsr].value !== 'null'
+        && tokens[tokenNamePwd].value !== null && tokens[tokenNamePwd].value !== 'null') {
+        tokenExistsCreds.value = true;
+        tokenvalUsr.value = tokens[tokenNameUsr].value;
+        tokenvalPwd.value = tokens[tokenNamePwd].value;
+    }
 });
 
 function goToHome() {
     window.location.href = window.location.pathname;
 }
 
+
+// File upload tokens
+function fileUploadFn() {
+    if (tokens[tokenNameUsr].value !== null && tokens[tokenNameUsr].value !== 'null'
+        && tokens[tokenNamePwd].value !== null && tokens[tokenNamePwd].value !== 'null') {
+        tokenExistsCreds.value = true;
+        tokenvalUsr.value = tokens[tokenNameUsr].value;
+        tokenvalPwd.value = tokens[tokenNamePwd].value;
+    }
+    tokenDialogCreds.value = true;
+    visibleUsr.value = false;
+    visiblePwd.value = false;
+}
+function cancelCreds() {
+    tokenvalUsr.value = '';
+    tokenvalPwd.value = '';
+    tokenDialogCreds.value = false;
+}
+function resetCreds() {
+    tokenvalUsr.value = '';
+    tokenvalPwd.value = '';
+    clearToken(tokenNameUsr);
+    clearToken(tokenNamePwd);
+}
+async function saveCreds() {
+    const { valid } = await tokenFormCreds.value.validate();
+    if (!valid) {
+        console.log('invalid');
+        return;
+    }
+    setToken(tokenNameUsr, tokenvalUsr.value);
+    setToken(tokenNamePwd, tokenvalPwd.value);
+    tokenDialogCreds.value = false;
+}
+
+
+// Service Token
 function tokenFn() {
     if (token.value !== null && token.value !== 'null') {
         tokenExists.value = true;
@@ -206,16 +334,14 @@ function tokenFn() {
     tokenDialog.value = true;
     visible.value = false;
 }
-
 function cancel() {
     tokenDialog.value = false;
 }
-
 function reset() {
     tokenval.value = '';
     http401response.value = false;
     customError.value = ''
-    clearToken();
+    clearToken(tokenName);
 }
 
 watch(
@@ -249,11 +375,13 @@ async function save() {
         console.log('invalid entry');
         return;
     }
-    setToken(tokenval.value);
+    setToken(tokenName, tokenval.value);
     http401response.value = false;
     customError.value = ''
     tokenDialog.value = false;
 }
+
+
 
 
 </script>
