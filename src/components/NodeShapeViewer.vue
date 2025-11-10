psychoinformatics-de/shacl-vue

commit d8a933f2c52e1cc8e87295f8180a103a11f95d5f

Allow constrained and user-controlled display of amount of related records
This pertains to both the 'NodeShapeViewer', which displays the literal nodes,
named nodes and blank nodes of a record being viewed, as well as the
'PropertyShapeEditor', which renders all form fields of a record being edited.
It is possible that some related nodes of a record will have a list of multiple
values. For example, a distribution might have many parts and it is bad UX if
e.g. 500 (or a million) parts are rendered automatically for a given distribution
record, both in the record viewer and record editor components.

For this reason, a new feature was introduced whereby a default number of records
will be rendered automatically, and the user will then be able to decide whether
to show more records, or to show fewer, by using dedicated buttons.

This was achieved as follows:
- new config options were introduced, one per viewer/editor:
  - 'editor_config[PropertyShapeEditor][recordNumberStepSize]' with a default of 5
  - 'viewer_config[NodeShapeViewer][recordNumberStepSize]' with a default of 5
- the config options define how many records to show by default, and the user can
  increase this to the max corresponding to the number of records, and to the min
  corresponding to the default stepsize
- the new UX code was added explicitly to the 'PropertyShapeEditor', and then via the
  new 'MoreOrLessRecordsViewer' to the 'NodeShapeViewer'

For this new functionality to work seamlessly with the 'PropertyShapeEditor's
ability to add/remove triples for a given form property, the instantiation of any
given editor component (via the 'component' tag) had to be updated. This was
necessary because the previous state did not use the 'key' of the component
correctly: it included the triple index where it should include the triple itself.
This is because the triple's position in the data array might change, but the
triple node itself won't change. So using the index in the 'key' actually lead to

src/components/NodeShapeViewer.vue
@@ -49,33 +49,39 @@
             </span>
         </v-card-subtitle>
         <v-card-text v-if="!props.formOpen">
-            <strong>Persistent Identifier</strong>: &nbsp;{{ record.title
-            }}<br />
+            <strong>Persistent Identifier</strong>: &nbsp;{{ record.title}}<br/>
+
             <!-- Literal nodes -->
             <span v-for="(v, k, index) in record.triples['Literal']">
                 <span v-if="propertyShapes[k]">
-                    <strong>{{
-                        nameOrCURIE(
-                            propertyShapes[k],
-                            shapesDS.data.prefixes,
-                            true
-                        )
-                    }}</strong
-                    >:
+                    <strong>
+                        {{
+                            nameOrCURIE(
+                                propertyShapes[k],
+                                shapesDS.data.prefixes,
+                                true
+                            )
+                        }}
+                    </strong>:
                 </span>
                 <span v-else>
-                    <strong>{{ k }}</strong
-                    >:
+                    <strong>{{ k }}</strong>:
                 </span>
                 <span v-for="(el, i) in v">
-                    <span v-if="v.length > 1"><br />&nbsp;-</span>
-                    &nbsp;<LiteralNodeViewer
-                        v-if="el.value"
-                        :textVal="el.value"
-                    ></LiteralNodeViewer>
+                    <span v-if="i < showCounts['Literal'][k]">
+                        <span v-if="v.length > 1"><br/>&nbsp;-</span>
+                        &nbsp;<LiteralNodeViewer v-if="el.value" :textVal="el.value"></LiteralNodeViewer>
+                    </span>
                 </span>
-                <br />
+                <br/>
+                <MoreOrLessRecordsViewer
+                    :records="v"
+                    v-model:count="showCounts['Literal'][k]"
+                    :stepSize="defaultStep"
+                ></MoreOrLessRecordsViewer>
             </span>
+
+
             <!-- Named nodes -->
             <span v-if="fetchingRecords">
                 <v-skeleton-loader type="paragraph"></v-skeleton-loader>
@@ -84,45 +90,54 @@
                 <span v-for="(v, k, index) in record.triples['NamedNode']">
                     <span v-if="k != RDF.type.value">
                         <span v-if="propertyShapes[k]">
-                            <strong>{{
-                                nameOrCURIE(
-                                    propertyShapes[k],
-                                    shapesDS.data.prefixes,
-                                    true
-                                )
-                            }}</strong
-                            >:
+                            <strong>
+                                {{
+                                    nameOrCURIE(
+                                        propertyShapes[k],
+                                        shapesDS.data.prefixes,
+                                        true
+                                    )
+                                }}
+                            </strong>:&nbsp;&nbsp;<MoreOrLessRecordsViewer
+                                :records="v"
+                                v-model:count="showCounts['NamedNode'][k]"
+                                :stepSize="defaultStep"
+                            ></MoreOrLessRecordsViewer>
                             <span v-for="(el, i) in v">
-                                <span v-if="v.length > 1"><br />&nbsp;-</span>
-                                &nbsp;<NamedNodeViewer
-                                    v-if="el.value"
-                                    :textVal="el.value"
-                                    :prefLabel="
-                                        getPrefLabel(el, rdfDS, allPrefixes)
-                                    "
-                                    :displayLabel="
-                                        getRecordDisplayLabel(el, rdfDS, allPrefixes, configVarsMain)
-                                    "
-                                    :quad="
-                                        getPidQuad(el.value, rdfDS.data.graph)
-                                    "
-                                    :targetClass="
-                                        propertyShapes[k][SHACL.class.value]
-                                    "
-                                >
-                                </NamedNodeViewer>
+                                <span v-if="i < showCounts['NamedNode'][k]">
+                                    <span v-if="v.length > 1"><br />&nbsp;-</span>
+                                    &nbsp;<NamedNodeViewer
+                                        v-if="el.value"
+                                        :textVal="el.value"
+                                        :prefLabel="
+                                            getPrefLabel(el, rdfDS, allPrefixes)
+                                        "
+                                        :displayLabel="
+                                            getRecordDisplayLabel(el, rdfDS, allPrefixes, configVarsMain)
+                                        "
+                                        :quad="
+                                            getPidQuad(el.value, rdfDS.data.graph)
+                                        "
+                                        :targetClass="
+                                            propertyShapes[k][SHACL.class.value]
+                                        "
+                                    >
+                                    </NamedNodeViewer>
+                                </span>
                             </span>
+                            
                         </span>
                         <span v-else>
                             <strong>{{ k }}</strong
                             >:
                             <span v-for="(el, i) in v">
-                                <span v-if="v.length > 1"><br />&nbsp;-</span>
-                                &nbsp;{{ el.value }}
+                                <span v-if="i < showCounts['NamedNode'][k]">
+                                    <span v-if="v.length > 1"><br />&nbsp;-</span>
+                                    &nbsp;{{ el.value }}
+                                </span>
                             </span>
                         </span>
-
-                        <br />
+                        <br>
                     </span>
                 </span>
             </span>
@@ -143,14 +158,18 @@
                 <span v-for="(v, k, index) in record.triples['BlankNode']">
                     <strong>{{
                         makeReadable(toCURIE(k, allPrefixes, 'parts').property)
-                    }}</strong
-                    >:
+                    }}</strong> ({{v.length}}):
                     <br />
                     <span v-for="(el, i) in v">
-                        <div>
+                        <div v-if="i < showCounts['BlankNode'][k]">
                             <BlankNodeViewer :node="el"></BlankNodeViewer>
                         </div>
                     </span>
+                    <MoreOrLessRecordsViewer
+                        :records="v"
+                        v-model:count="showCounts['BlankNode'][k]"
+                        :stepSize="defaultStep"
+                    ></MoreOrLessRecordsViewer>
                 </span>
             </span>
         </v-card-text>
@@ -212,6 +231,7 @@ import {
     getRecordDisplayLabel,
 } from '../modules/utils';
 import { RDF, SHACL } from '@/modules/namespaces';
+import MoreOrLessRecordsViewer from './MoreOrLessRecordsViewer.vue';
 // Define component properties
 const props = defineProps({
     classIRI: String,
@@ -235,6 +255,15 @@ const propertyShapes = {};
 for (var p of shape_obj.properties) {
     propertyShapes[p[SHACL.path.value]] = p;
 }
+const defaultStep = configVarsMain.viewerConfig?.NodeShapeViewer?.recordNumberStepSize;
+const showBlankNodeCounts = reactive({});
+const showCounts = reactive(
+    {
+        'Literal': {},
+        'NamedNode': {},
+        'BlankNode': {},
+    }
+);
 
 console.log(propertyShapes);
 
@@ -261,10 +290,12 @@ onBeforeMount(async () => {
     if (configVarsMain['idResolvesExternally'].indexOf(recordPIDprefix) >= 0) {
         resolveExternally.value = true;
     }
+    initShowCounts();
 });
 
 onUpdated(() => {
     updateRecord(false);
+    initShowCounts();
 });
 
 async function viewRDF() {
@@ -284,6 +315,16 @@ function downloadTTL() {
 
 function showHideBlankNodes() {
     showBlankNodes.value = !showBlankNodes.value;
+}
+
+function initShowCounts() {
+    for (const n of ['BlankNode', 'NamedNode', 'Literal']) {
+        for (const pred in record.triples[n]) {
+            if (!showCounts[n].hasOwnProperty(pred)) {
+                showCounts[n][pred] = defaultStep;
+            }
+        }
+    }
 }
 
 function getRecordQuads() {

src/components/MoreOrLessRecordsViewer.vue
@@ -1,0 +1,66 @@
+<template>
+    <span v-if="records.length > count">
+        <v-tooltip text="Show more..." location="top">
+            <template v-slot:activator="{ props }">
+                <v-btn
+                    v-bind:="props"
+                    no-gutters
+                    @click="increase()"
+                    density="compact"
+                    icon="mdi-plus"
+                    size="small"
+                    variant="tonal"
+                    rounded="sm"
+                ></v-btn>
+            </template>
+        </v-tooltip>
+    </span>
+    <span v-if="count > stepSize">
+        &nbsp;
+        <v-tooltip text="Show less..." location="top">
+            <template v-slot:activator="{ props }">
+                <v-btn
+                    v-bind:="props"
+                    no-gutters
+                    @click="decrease()"
+                    density="compact"
+                    icon="mdi-minus"
+                    size="small"
+                    variant="tonal"
+                    rounded="sm"
+                ></v-btn>
+            </template>
+        </v-tooltip>
+    </span>
+    <span v-if="records.length > stepSize">
+        &nbsp; <em>(showing {{ count }} of {{ records.length }})</em>
+    </span>
+</template>
+
+<script setup>
+const props = defineProps({
+    records: Array,
+    count: Number,
+    stepSize: Number,
+
+});
+
+const emit = defineEmits(['update:count']);
+
+function increase() {
+    const newTempCount = props.count + props.stepSize
+    const newCount = newTempCount > props.records.length ? props.records.length : newTempCount
+    emit('update:count', newCount);
+}
+
+function decrease() {
+    const newCount = props.count - props.stepSize < props.stepSize
+        ? props.stepSize
+        : props.count - props.stepSize;
+    emit('update:count', newCount);
+}
+</script>
+
+<style scoped>
+
+</style>

src/components/PropertyShapeEditor.vue
@@ -40,9 +40,9 @@
                             v-for="(triple, triple_idx) in formData.content[
                                 localNodeUid
                             ][localNodeIdx][my_uid]"
-                            :key="localNodeUid + '-' + my_uid + '-' + triple_idx"
+                            :key="localNodeUid + '-' + my_uid + '-' + triple"
                         >
-                            <v-col cols="9" class="d-flex align-center" @click.stop="showTooltip = false" @mouseenter="showTooltip = false">      
+                            <v-col v-if="triple_idx < currentCount" cols="9" class="d-flex align-center" @click.stop="showTooltip = false" @mouseenter="showTooltip = false">      
                                 &nbsp;              
                                 <Suspense>
                                     <template #default>
@@ -59,6 +59,7 @@
                                                 :triple_uid="my_uid"
                                                 :triple_idx="triple_idx"
                                                 :disabled="compDisabled"
+                                                
                                                 @click.stop="showTooltip = false"
                                                 @focus="showTooltip = false"
                                                 @focusin="showTooltip = false"
@@ -73,7 +74,7 @@
                                     </template>
                                 </Suspense>
                             </v-col>
-                            <v-col>
+                            <v-col v-if="triple_idx < currentCount">
                                 &nbsp;
                                 <!-- Remove button -->
                                 <v-btn
@@ -82,7 +83,7 @@
                                     elevation="1"
                                     icon="mdi-delete-outline"
                                     @click.stop="
-                                        formData.removeObject(
+                                        removeTriple(
                                             localNodeUid,
                                             localNodeIdx,
                                             my_uid,
@@ -100,10 +101,11 @@
                                     elevation="1"
                                     icon="mdi-plus-circle-outline"
                                     @click.stop="
-                                        formData.addObject(
+                                        addTriple(
                                             localNodeUid,
                                             localNodeIdx,
-                                            my_uid
+                                            my_uid,
+                                            triple_idx
                                         )
                                     "
                                     density="comfortable"
@@ -111,6 +113,45 @@
                                 ></v-btn>
                             </v-col>
                         </v-row>
+                        <div
+                            v-if="formData.content[localNodeUid][localNodeIdx][my_uid] && (
+                                formData.content[localNodeUid][localNodeIdx][my_uid].length > currentCount ||
+                                formData.content[localNodeUid][localNodeIdx][my_uid].length > defaultStep
+                            )"
+                            class="d-flex justify-start"
+                            style="padding-left: 0.3em;"
+                        >
+                            <span v-if="formData.content[localNodeUid][localNodeIdx][my_uid].length > currentCount">
+                                <v-tooltip text="Show more..." location="top">
+                                    <template v-slot:activator="{ props }">
+                                        <v-btn
+                                            v-bind:="props"
+                                            no-gutters
+                                            @click="currentCount+=defaultStep"
+                                            density="compact"
+                                            icon="mdi-plus"
+                                            size="small"
+                                        ></v-btn>
+                                    </template>
+                                </v-tooltip>
+                            </span>
+                            <span v-if="currentCount > defaultStep">
+                                &nbsp;
+                                <v-tooltip text="Show less..." location="top">
+                                    <template v-slot:activator="{ props }">
+                                        <v-btn
+                                            v-bind:="props"
+                                            no-gutters
+                                            @click="currentCount = currentCount - defaultStep < defaultStep ? defaultStep : currentCount - defaultStep"
+                                            density="compact"
+                                            icon="mdi-minus"
+                                            size="small"
+                                        ></v-btn>
+                                    </template>
+                                </v-tooltip>
+                            </span>
+                            &nbsp; <em>(showing {{ currentCount }} of {{ formData.content[localNodeUid][localNodeIdx][my_uid].length }})</em>
+                        </div>
                     </span>
                 </v-col>
             </v-row>
@@ -188,6 +229,8 @@ const configVarsMain = inject('configVarsMain');
 const ID_IRI = inject('ID_IRI');
 const compDisabled = ref(false);
 const configMatchedComponent = ref(null);
+const defaultStep = configVarsMain.editorConfig?.PropertyShapeEditor?.recordNumberStepSize;
+const currentCount = ref(defaultStep)
 
 // ----------------- //
 // Lifecycle methods //
@@ -343,13 +386,27 @@ function allowAddTriple(idx) {
         if (
             formData.content[localNodeUid.value][localNodeIdx.value][
                 my_uid.value
-            ].length ==
-            idx + 1
+            ].length == idx + 1
         ) {
             return true;
-        } else {
+        } else if (
+            (currentCount.value == idx + 1) &&
+            currentCount.value < formData.content[localNodeUid.value][localNodeIdx.value][
+                my_uid.value
+            ].length
+        ) {
+            return true;
+        }
+        else {
             return false;
         }
+    }
+}
+
+function addTriple(class_uri, subject_uri, predicate_uri, current_idx) {
+    formData.content[class_uri][subject_uri][predicate_uri].splice(current_idx+1, 0, null);
+    if (current_idx+1==currentCount.value) {
+        currentCount.value+=1;
     }
 }
 
@@ -361,6 +418,15 @@ function allowRemoveTriple(idx) {
         return true;
     }
     return false;
+}
+
+function removeTriple(class_uri, subject_uri, predicate_uri, current_idx) {
+    formData.removeObject(
+        class_uri,
+        subject_uri,
+        predicate_uri,
+        current_idx
+    )
 }
 </script>
 

src/composables/configuration.js
@@ -28,8 +28,17 @@ const mainVarsToLoad = {
             yearEnd: 2077
         },
         InstancesSelectEditor: {
-            fetchingsRecordsText: "Fetching records..."
-        }
+            fetchingsRecordsText: "Fetching records...",
+        },
+        PropertyShapeEditor: {
+            recordNumberStepSize: 5,
+        },
+    },
+    viewer_config: {
+        NodeShapeViewer: {
+            recordNumberStepSize: 5,
+        },
+
     },
     display_name_autogenerate: {},
     display_name_autogenerate_placeholder: {

commit 70a10a2479eea8d614c2dbf83847e857e50fbde7

Fix 'NodeShapeViewer' update issue
This issue was introduced in d8a933f2c52e1cc8e87295f8180a103a11f95d5f with the
functionality to allow constrained and user-controlled display of the amount of
related records of a particular kind in the 'NodeShapeViewer'. The problem had
two parts:
- 'onUpdated' for the 'NodeShapeViewer' was not being called as expected every
  time a form is saved, which meant the record was not updated from the most
  recent state of the graph
- 'initShowCounts' is crucial to set reactively, because there are conditional
  rendering clauses in the template that depend on it; this is why we did not see
  updated values for edited fields.
The fix now uses 'watch' the react to 'lastSavedNode'. If the last saved node is
the same as the current 'NodeShapeViewer' instance, the 'updateRecord' and
'initShowCounts' functions are called and edits are visible.

src/components/NodeShapeViewer.vue
@@ -267,10 +267,8 @@ import {
     reactive,
     onBeforeMount,
     inject,
-    onUpdated,
     ref,
-    nextTick,
-    toRaw,
+    watch,
     provide,
     computed,
 } from 'vue';
@@ -288,7 +286,6 @@ import {
 } from '../modules/utils';
 import { RDF, SHACL } from '@/modules/namespaces';
 import MoreOrLessRecordsViewer from './MoreOrLessRecordsViewer.vue';
-import TextOrLinkViewer from './TextOrLinkViewer.vue';
 // Define component properties
 const props = defineProps({
     classIRI: String,
@@ -304,6 +301,7 @@ const fetchFromService = inject('fetchFromService');
 const getClassIcon = inject('getClassIcon');
 const rdfDS = inject('rdfDS');
 const shapesDS = inject('shapesDS');
+const lastSavedNode = inject('lastSavedNode');
 const record = reactive({});
 const showBlankNodes = ref(false);
 const shape_obj = shapesDS.data.nodeShapes[props.classIRI];
@@ -339,7 +337,6 @@ function selectNamedNode(recordClass, recordPID) {
 provide('selectNamedNode', selectNamedNode);
 
 onBeforeMount(async () => {
-
     if (configVarsMain.allowCopyRecordUrls === true ||
         ( Array.isArray(configVarsMain.allowCopyRecordUrls) &&
         configVarsMain.allowCopyRecordUrls.indexOf(props.classIRI) >= 0 )
@@ -356,13 +353,6 @@ onBeforeMount(async () => {
     }
     initShowCounts();
 });
-
-onUpdated(() => {
-    updateRecord(false);
-    initShowCounts();
-});
-
-
 
 const specialBlankNodes = computed( () => {
     const triples = record.triples?.['BlankNode'] ?? {};
@@ -460,8 +450,7 @@ async function updateRecord(fetchData) {
     for (const rQ of record.relatedQuads) {
         await addRecordProperty(rQ, fetchData);
     }
-    record.displayLabel = getRecordDisplayLabel(record.quad.subject, rdfDS, allPrefixes, configVarsMain)
-    
+    record.displayLabel = getRecordDisplayLabel(record.quad.subject, rdfDS, allPrefixes, configVarsMain)    
     // Now we have all record.triples, and we need to get displaylabels for blanknodes
     for (const triplePred in record.triples['BlankNode']) {
         let cIRI = propertyShapes[triplePred][SHACL.class.value];
@@ -500,6 +489,18 @@ async function addRecordProperty(quad, fetchData) {
     }
     record.triples[termType][quad.predicate.value].values.push(quad.object);
 }
+
+// trigger record update whenever lastSavedNode is updated, i.e. whenever a form is saved
+watch(
+    lastSavedNode, async (savedNode) => {
+        if (savedNode) {
+            if (savedNode.node_iri == record.value) {
+                await updateRecord(false, 'lastSavedNode')
+                initShowCounts();
+            }
+        }
+    }
+)
 
 function copyRecordLink() {
     var nodeShapeCurie = toCURIE(props.classIRI, allPrefixes);